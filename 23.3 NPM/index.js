daa6689bd0bcadd322aa8883cf9d3018787a5922
Jaskirat Singh <144475226+JassTxM@users.noreply.github.com>
2025-02-09 20:01:14 +0530

    Add Module 23 Files

---
+++ 23.3 NPM/index.js
@@ -0,0 +1,10 @@
+// var generateName = require('sillyname');
+
+// import generateName from "sillyName";
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`);
+
+import superheroes from "superheroes";
+var sh = superheroes.random();
+console.log(`I am Superhero , named ${sh}.`);
